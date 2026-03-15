872a38c0829f5c1b294361cb228e66743a91fa67
ducanhdhtb06-hub <ducanhdhtb06@gmail.com>
2026-03-15 12:05:52 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,46 +1,44 @@
-## 
-import torch
-from torch.utils.data import Dataset, DataLoader
 import os
 import pickle
-import cv2
 import numpy as np
+import torch
+from torch.utils.data import Dataset
+import cv2
+from torchvision.transforms import ToTensor
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+from torchvision.transforms.functional import to_tensor
+from torchvision.transforms import ToTensor, Resize , Compose
 from PIL import Image
-from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 
 
-
-class AnimalDataset(Dataset):
-    def __init__(self, root, train=True, transform=None):
-        self.image_paths = []
-        self.labels = []
-        self.categories = ["butterfly", "cat", "chicken", "cow", "dog", "elephant", "horse", "sheep", "spider",
-                           "squirrel"]
+class AnimaDataset(Dataset) :
+    def __init__(self, root, train = True, transform = None) :
         self.transform = transform
-
-        data_path = os.path.join(root, "animals")
-
-        if train:
-            data_path = os.path.join(data_path, "train")
-        else:
-            data_path = os.path.join(data_path, "test")
-
-        for i, category in enumerate(self.categories):
-            data_files = os.path.join(data_path, category)
-            for item in os.listdir(data_files):
-                path = os.path.join(data_files, item)
-                self.image_paths.append(path)
+        if train :
+            mode = "train"
+            self.root = os.path.join(root, mode)
+        else :
+            mode = "test"
+            self.root = os.path.join(root, mode)
+        self.categories = ['butterfly', 'cat', 'chicken', 'cow', 'dog', 'elephant', 'horse', 'sheep', 'spider', 'squirrel']
+        self.images_path = []
+        self.labels = []
+        for i , category in enumerate( self.categories):
+            data_file_path = os.path.join(self.root, category)
+            for file_name in os.listdir(data_file_path):
+                file_path = os.path.join(data_file_path, file_name)
+                self.images_path.append(file_path)
                 self.labels.append(i)
 
     def __len__(self):
         return len(self.labels)
 
-    def __getitem__(self, index):
-        image_path = self.image_paths[index]
-        image = Image.open(image_path).convert('RGB')
-        # image = cv2.imread(image_path)
-        # image = torch.from_numpy(image)
-        label = self.labels[index]
+    def __getitem__(self, idx) :
+        image_path = self.images_path[idx]
+        image = Image.open(image_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
-        return image, label
+        label = self.labels[idx]
+        return image , label
+
