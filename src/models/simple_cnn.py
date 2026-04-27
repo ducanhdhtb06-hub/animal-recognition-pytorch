7b93f9e20319b72a67b6c4a7ddfe9200f5dd5b36
ducanhdhtb06-hub <ducanhdhtb06@gmail.com>
2026-04-27 14:37:40 +0700

    Update simple_cnn.py

--- src/models/simple_cnn.py
+++ src/models/simple_cnn.py
@@ -1,174 +1,46 @@
-import os.path
-import shutil
-
-from torch.utils.checkpoint import checkpoint
-
-from AnimaDataset import AnimaDataset
-from simplenetwork import simpleCNN
-from torch.utils.data import DataLoader , Dataset
-import torch.nn as nn
-from torchvision.transforms import Compose, Resize, ToTensor, Normalize, RandomAffine, ColorJitter
-import torch.optim as optim
 import torch
-from sklearn.metrics import classification_report, accuracy_score , confusion_matrix
-from torch.utils.tensorboard import SummaryWriter
-from argparse import ArgumentParser
-import shutil
-from tqdm import tqdm
-import numpy as np
-import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch
+class simpleCNN(nn.Module):
+    def __init__(self, num_classes = 10):
+        super().__init__()
+        self.conv1 = nn.Sequential(self.make_block(in_channels=3, out_channels=8),
+                     self.make_block(in_channels=8, out_channels=16),
+                     self.make_block(in_channels=16, out_channels=32),
+                     self.make_block(in_channels=32, out_channels=64),
+                     self.make_block(in_channels=64, out_channels=128))
+        self.Flatten = nn.Flatten()
+        self.fc1  = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(in_features=6272, out_features=512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=512, out_features=1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=1024, out_features=10))
 
-def get_args():
-    parser = ArgumentParser("CNN")
-    parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
-    parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
-    parser.add_argument("--image_size", type=int, default=224, help="size of each image dimension")
-    parser.add_argument("--r", type=str, default="animals_v2/animals", help="root path of dataset")
-    parser.add_argument("--checkpoint", type=str, default="train_models/last_model.pt", help="root path of dataset")
-    args = parser.parse_args()
-    return args
-
-def plot_confusion_matrix(writer, cm, class_names, epoch):
-
-    figure = plt.figure(figsize=(20, 20))
-    plt.imshow(cm, interpolation='nearest', cmap="Blues")
-    plt.title("Confusion matrix")
-    plt.colorbar()
-
-    tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=45)
-    plt.yticks(tick_marks, class_names)
-
-    cm = cm.astype('float') / np.maximum(cm.sum(axis=1)[:, np.newaxis], 1)
-    cm = np.around(cm, 2)
-
-    threshold = cm.max() / 2.
-
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            color = "white" if cm[i, j] > threshold else "black"
-            plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-    writer.add_figure('confusion_matrix', figure, epoch)
-
-    os.makedirs("outputs", exist_ok=True)
-    figure.savefig("outputs/confusion_matrix.png")
-
-    plt.close(figure)
+    def make_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding="same", stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding="same", stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2, 2))
+    def forward(self , x):
+        x = self.conv1(x)
+        x= self.Flatten(x)
+        x = self.fc1(x)
+        return x
 if __name__ == '__main__':
-    num_epochs = 100
+    model = simpleCNN()
+    input_data = torch.randn(8 , 3 ,224,224)
+    model.cuda()
+    input_data = input_data.cuda()
+    while True :
+        output = model(input_data)
+        print(output.shape)
+        break
 
-    args = get_args()
-    train_transform = Compose([
-        RandomAffine(
-            degrees=(-5, 5),
-            translate=(0.15, 0.15),
-            scale=(0.85, 1.15),
-            shear=5
-        ),
-        Resize((args.image_size, args.image_size)),
-        ColorJitter(
-            brightness=0.125,
-            contrast=0.5,
-            saturation=0.4,
-            hue=0.1
-        ),
-        ToTensor(),
-    ])
-    test_transform = Compose([
-        Resize((args.image_size, args.image_size)),
-        ToTensor(),
-    ])
-    train_dataset = AnimaDataset(root=args.r, train=True, transform=train_transform)
-    train_loader = DataLoader(
-        dataset = train_dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=2,
-        drop_last=True)
-    test_dataset = AnimaDataset(root=args.r, train=False, transform=test_transform)
-    test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=args.batch_size,
-        shuffle=False,
-        num_workers=2,
-        drop_last=False)
-    model = simpleCNN(num_classes=10)
-    train_models = "train_models"
-
-
-    if os.path.isdir("tensorboard"):
-        shutil.rmtree("tensorboard")
-    if not os.path.isdir(train_models):
-        os.mkdir(train_models)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    writer = SummaryWriter("tensorboard")
-    if torch.cuda.is_available():
-        model.cuda()
-
-    if args.checkpoint:
-        checkpoint = torch.load(args.checkpoint , weights_only=False)
-        model.load_state_dict(checkpoint["state_dict"])
-        start_epoch = checkpoint["epoch"]
-        optimizer.load_state_dict(checkpoint["optimizer"])
-        best_accuracy = checkpoint["best_accuracy"]
-    else :
-        start_epoch = 0
-        best_accuracy = 0
-
-    for epoch in range(  start_epoch , args.epochs):
-        model.train()
-        progress_bar = tqdm(train_loader , colour= "green")
-        for iters , (images , labels) in enumerate(progress_bar):
-            if torch.cuda.is_available():
-                images = images.cuda()
-                labels = labels.cuda()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            writer.add_scalar('train/loss', loss, epoch * len(train_loader) + iters)
-            progress_bar.set_description("Epoc {}/{}. Iteration {}/{}. loss{}".format(epoch+1, args.epochs, iters + 1, len(train_loader), loss))
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        model.eval()
-        all_predictions = []
-        all_labels = []
-
-        for iters, (images, labels) in enumerate(test_loader):
-            all_labels.extend(labels)
-            if torch.cuda.is_available():
-                images = images.cuda()
-                labels = labels.cuda()
-            with torch.no_grad():
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                indices = torch.argmax(outputs.cpu(), 1)
-                all_predictions.extend(indices)
-        all_labels = [label.item() for label in all_labels]
-        all_predictions = [indices.item() for indices in all_predictions]
-
-        plot_confusion_matrix(writer, confusion_matrix(all_labels, all_predictions),
-                              class_names=test_dataset.categories, epoch=epoch)
-        print("Epoch {}: Accurancy: {}".format(epoch+1, accuracy_score(all_labels, all_predictions)))
-        writer.add_scalar('test/accuracy', accuracy_score(all_labels, all_predictions), epoch+1)
-        checkpoint = {
-            'epoch': epoch + 1,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'best_accuracy': accuracy_score(all_labels, all_predictions)
-        }
-        torch.save(checkpoint, "{}/last_model.pt".format(train_models))
-        if best_accuracy < accuracy_score(all_labels, all_predictions):
-            best_accuracy = accuracy_score(all_labels, all_predictions)
-            checkpoint = {
-                'epoch': epoch + 1,
-                'state_dict': model.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'best_accuracy': best_accuracy,
-            }
-            torch.save(checkpoint, "{}/best_model.pt".format(train_models))
